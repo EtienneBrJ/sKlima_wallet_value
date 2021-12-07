7fdbb2c4fa51aee98b7aca1b7b4212322a46007d
EtienneBrJ <etiennebrxv@gmail.com>
2021-12-07 23:28:12 +0100

    Display plot in flask App

--- graph.py
+++ graph.py
@@ -1,5 +1,7 @@
 """ Graph module
-    ...
+
+    Iterate in a MongoDB collection and save the date and the price in two separate arrays
+    Generate a plot with these arrays and save it as an image in the static folder
     ...
 """
 import numpy as np
@@ -10,23 +12,25 @@ import os
 
 load_dotenv()
 
-x_data = []
-y_data = []
 
 client = pymongo.MongoClient(
     'mongodb+srv://titi:' + os.environ.get('password') + '@cluster0.xfvlr.mongodb.net/KlimaDAO?retryWrites=true&w=majority')
 db = client.KlimaDAO
 sKlimaCol = db.sKlimaWallet
 
-for document in sKlimaCol.find({}, {'_id': 0}):
-    # Put in two list separately the date and the usd_value to display a graph
-    print(document.get('date'))
-    x_data.append(document.get('date'))
-    y_data.append(document.get('usd_value'))
 
+def getPlot():
 
-x = np.array(x_data)
-y = np.array(y_data)
+    x_data = []
+    y_data = []
+    ite = 0
 
-plt.plot(x, y)
-plt.show()
+    for document in sKlimaCol.find({}, {'_id': 0}):
+        ite += 1
+        x_data.append(ite)
+        y_data.append(document.get('usd_value'))
+
+    x = np.array(x_data)
+    y = np.array(y_data)
+    plt.plot(x, y)
+    plt.savefig('static/sKlimaPlot.png')
